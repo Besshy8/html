e27535b1f8fd34825b1c2a81e42ba3739982aa48
toshiki yasuda <besshy@toshikinoMacBook-Pro.local>
2019-10-09 16:54:47 +0900

    Add fadeOut

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@ $(function(){
     $("#table-btn").click(function(){
         $("#login-modal").fadeIn();
     });
+
+    $("#submit-btn").click(function(){
+        $("#login-modal").fadeOut();
+    })
 });
